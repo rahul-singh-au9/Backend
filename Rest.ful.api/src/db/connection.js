cc69a8e850d92d7256b791e18730df8807ae965b
Rahul singh <rahulsg1508@gmail.com>
2021-08-16 11:10:13 +0530

    latest commit

--- Rest.ful.api/src/db/connection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://localhost:27017/students-api", {
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-})
-.then(() => console.log("connection successfull...."))
-.catch((err) => console.log(err));
